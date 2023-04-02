bbd04cfd1eb15eb86fd2d11dd3831f916fe82b4c
Irshal Firdeansyah <irshalfir@gmail.com>
2023-04-02 18:25:38 +0700

    implement ktx to navigate with fragment manager

--- app/src/main/java/com/dicoding/fundamental/fragment/MainActivity.kt
+++ app/src/main/java/com/dicoding/fundamental/fragment/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dicoding.fundamental.fragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +17,9 @@ class MainActivity : AppCompatActivity() {
 
         if (fragment !is HomeFragment) {
             Log.d("MyFlexibleFragment", "Fragment Name: ${HomeFragment::class.java.simpleName}")
-            fragmentManager
-                .beginTransaction()
-                .add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
-                .commit()
+            fragmentManager.commit {
+                add(R.id.frame_container, homeFragment, HomeFragment::class.java.simpleName)
+            }
         }
 
     }
